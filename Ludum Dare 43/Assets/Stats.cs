19d960c12f254a90f995f5956f02c11d9c86b920
brendonky18 <brendonky18@users.noreply.github.com>
2018-12-01 02:40:54 -0500

    Began implementing various stats

---
+++ Ludum Dare 43/Assets/Stats.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour {
+    //all the stats that I could think of
+    private Dictionary<string, float> playerStats = new Dictionary<string, float> {
+        {"Grades", 0.5f },
+        {"Money", 1000f },
+        {"Friends", 10f },
+        {"Family", 0.5f },
+        {"Social Life", 0.5f },
+        {"Health", 0.9f },
+        {"Stress", 0.0f },
+        {"Happiness", 0.5f },
+        {"Debt", 0f },
+    };
+}
